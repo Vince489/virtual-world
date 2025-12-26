1be1cf881650a2c08c2feaedd2e02c3cdb06499f
Vince489 <vincentcornelius489@gmail.com>
2025-12-26 15:49:03 -0500

    added mongodb

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import connectDB from './config/db.js';
 
 const app = express();
 
@@ -8,6 +9,8 @@ const PORT = process.env.PORT;
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
